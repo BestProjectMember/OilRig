2b2309cf5d7dfe47bf5319dbd138dcf7e2ba01a3
Peipei Su <266128@via.dk>
2019-10-24 15:46:43 +0200

    Fire extinguisher functions, smoke effects

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+	public ParticleSystem smoke;
+	public GameObject burningObject;
+	private bool haveSmoked;
+    // Start is called before the first frame update
+    void Start()
+    {
+		haveSmoked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnParticleCollision(GameObject other)
+	{
+		//	if(other.gameObject.name == "Foam")
+		//	{
+		if (gameObject.transform.localScale.x > 0)
+		{
+			Debug.Log("collision enter");
+			StartCoroutine("PutOut");
+		}
+
+		if(gameObject.transform.localScale.x < 0 && !haveSmoked)
+		{
+			StartCoroutine("Smoke");
+			haveSmoked = true;
+		}
+	//	}
+	}
+
+	IEnumerator PutOut()
+	{
+		gameObject.transform.localScale += new Vector3(-0.01f, -0.01f, -0.01f);
+		Debug.Log("fire scale -0.1");
+		yield return new WaitForSeconds(1f);
+	}
+
+	IEnumerator Smoke()
+	{
+		smoke.Play();
+		yield return new WaitForSeconds(5f);
+		smoke.Stop();
+		burningObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/FireExtinguisher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.Interactions.Interactables;
+
+public class FireExtinguisher : MonoBehaviour
+{
+	public Transform handleTransform;
+	public GameObject body;
+	public InteractableFacade interactableFacade;
+	public ParticleSystem foam;
+	public GameObject nozzle;
+	bool triggerIsPressed; //value to detect if the trigger is already pressed, in case users press both left and right trigger to active foam
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		triggerIsPressed = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		foam.transform.position = nozzle.transform.position;
+
+		//if player grabbs the fire extinguisher but looses hand
+		if (!interactableFacade.IsGrabbed && triggerIsPressed)
+		{
+			Stop();
+		}
+	}
+
+	//if player press the handle
+	public void Active()
+	{
+		if (interactableFacade.IsGrabbed && !triggerIsPressed)
+		{
+			handleTransform.Rotate(0, 0, 90);
+			foam.Play();
+			triggerIsPressed = true;
+		}
+	}
+
+	//if player loose the handle
+	public void Stop()
+	{
+		if (interactableFacade.IsGrabbed && triggerIsPressed)
+		{
+			handleTransform.Rotate(0, 0, -90);
+			foam.Stop();
+			triggerIsPressed = false;
+		}
+	}
+}
