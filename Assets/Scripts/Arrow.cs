3c8ab35e4a4a51ea4b2ccddf8fe166e87a416338
LukasJOfficial <54264562+LukasJOfficial@users.noreply.github.com>
2019-10-24 15:06:19 +0200

    First version of arrow

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    GameObject target = null;
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target.transform);
+        }
+
+        else
+            gameObject.SetActive(false);
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+
+    //public void SetTarget(string tag)
+    //{
+    //    this.target = GameObject.FindGameObjectWithTag(tag);
+    //}
+
+    //private void OnEnable()
+    //{
+    //    ClipboardControl.tagChanged += TagChangeEvent;
+    //    if (control.GetCurrentTag() != null)
+    //    {
+    //        SetTarget(control.GetCurrentTag());
+    //    }
+    //}
+    //private void OnDisable()
+    //{
+    //    ClipboardControl.tagChanged -= TagChangeEvent;
+    //}
+
+    //void TagChangeEvent(string tag)
+    //{
+    //    if (tag == null)
+    //    {
+    //        target = null;
+    //        return;
+    //    }
+    //    SetTarget(tag);
+    //}
+}
+
--- Assets/Scripts/ManageScenes.cs
+++ Assets/Scripts/ManageScenes.cs
@@ -10,3 +10,4 @@ public class ManageScenes : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 }
+
