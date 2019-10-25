ee99e53cf2f1546a7498b7fd855667686afb1ff6
Peipei Su <266128@via.dk>
2019-10-25 15:28:29 +0200

    damaged object throwable

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class Fire : MonoBehaviour
 {
 	public ParticleSystem smoke;
-	public GameObject burningObject;
 	public GameObject snapDropZone;
+	public GameObject burningObject;
+	public Rigidbody burningObjectRigidbody;
 	private bool haveSmoked;
+	public bool objectThrowable;
+
     // Start is called before the first frame update
     void Start()
     {
 		haveSmoked = false;
+		objectThrowable = false;
+		burningObjectRigidbody.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	//	gameObject.transform.position = Vector3.Lerp(transform.position,burningObject.transform.position,Time.time);
     }
 
 	void OnParticleCollision(GameObject other)
 	{
 		if (gameObject.transform.localScale.x > 0)
 		{
-			Debug.Log("collision enter");
 			StartCoroutine("PutOut");
 		}
 
@@ -38,7 +42,6 @@ public class Fire : MonoBehaviour
 	IEnumerator PutOut()
 	{
 		gameObject.transform.localScale += new Vector3(-0.01f, -0.01f, -0.01f);
-		Debug.Log("fire scale -0.1");
 		yield return new WaitForSeconds(1f);
 	}
 
@@ -47,7 +50,7 @@ public class Fire : MonoBehaviour
 		smoke.Play();
 		yield return new WaitForSeconds(5f);
 		smoke.Stop();
-		burningObject.SetActive(false);
-		snapDropZone.SetActive(true);
+	//	burningObjectRigidbody.isKinematic = !burningObjectRigidbody.isKinematic;
+		objectThrowable = true;
 	}
 }
---
+++ Assets/Scripts/ThrowingPlace.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingPlace : MonoBehaviour
+{
+	public GameObject cube;
+	public GameObject snapDropZone;
+	public Fire fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (fire.objectThrowable)
+		{
+			cube.SetActive(false);
+			snapDropZone.SetActive(true);
+		}
+	}
+}
