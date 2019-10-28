3bf8f5966d827f2a351f77184aef2c5bcdf755ab
Peipei Su <266128@via.dk>
2019-10-28 10:27:34 +0100

    clean2

---
+++ Assets/Scripts/ChangePosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour
+{
+    public Transform Target;
+    public Transform destination;
+    
+    public void Move()
+    {
+        Target.transform.position = destination.transform.position;
+    }
+}
