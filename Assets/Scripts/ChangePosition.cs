bf1300099a88aa262b0a48c2e8d49502e383fe01
Smitchy <life.lime@gmail.com>
2019-10-28 10:34:01 +0100

    Revert "project folder clean"
    This reverts commit a2c0842469814ef32e8d8dfca9d4a31833115538.

---
+++ Assets/Scripts/ChangePosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour
+{
+    public GameObject target;
+
+    public void Move(GameObject moveLocation)
+    {
+        target.transform.position = moveLocation.transform.position;
+    }
+}
---
+++ Assets/Scripts/ManageScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour
+{
+    public void ChangeSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
