76c26648714062d7bab438fb46d04a510f56f626
LukasJOfficial <54264562+LukasJOfficial@users.noreply.github.com>
2019-11-05 09:57:53 +0100

    setLocation method added

--- Assets/Scripts/NavMeshScripts/PlayerController.cs
+++ Assets/Scripts/NavMeshScripts/PlayerController.cs
@@ -14,31 +14,21 @@ public class PlayerController : MonoBehaviour
     {
         player = gameObject.GetComponent<Transform>();
         path = gameObject.GetComponent<DrawAgentPath>();
-        //location.transform.position = player.position;
     }
     // Update is called once per frame
     void Update()
     {
-        //if(Input.GetMouseButtonDown(0))
-        //{
-        //    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        //    RaycastHit hit;
-            
-
-        //    if(Physics.Raycast(ray, out hit))
-        //    {
-        //        //agent.SetDestination(hit.point);
-        //        location.transform.position = hit.point;
-        //        //path.getPath(hit.point);
-        //    }
-        //}
         if(location != null)
         {
             if(player.position != location.transform.position)
             {
                 path.getPath(location.transform.position);
             }
-        }
-            
+        }    
+    }
+
+    public void setLocation(GameObject location)
+    {
+        this.location = location;
     }
 }
