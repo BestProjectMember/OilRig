04374b70a7cbd599e754707e6b843faa6efd380d
LukasJOfficial <54264562+LukasJOfficial@users.noreply.github.com>
2019-11-05 09:52:01 +0100

    (Not yikes) navigation system kinda works

--- Assets/Scripts/NavMeshScripts/DrawAgentPath.cs
+++ Assets/Scripts/NavMeshScripts/DrawAgentPath.cs
@@ -9,8 +9,8 @@ public class DrawAgentPath : MonoBehaviour
     //Transform target; //to hold the transform of the target
     NavMeshAgent agent; //to hold the agent of this gameObject
     public Material lineMaterial;
-    public GameObject arrow;
-    public GameObject arrowParent;
+    //public GameObject arrow;
+    //public GameObject arrowParent;
     Transform temp;
 
     void Start()
@@ -22,7 +22,7 @@ public class DrawAgentPath : MonoBehaviour
 
     public void getPath(Vector3 target)
     {
-        //line.SetPosition(0, transform.position); //set the line's origin
+        line.SetPosition(0, transform.position); //set the line's origin
         agent.SetDestination(target); //create the path
         NavMeshPath path = agent.path;
 
@@ -33,13 +33,13 @@ public class DrawAgentPath : MonoBehaviour
     IEnumerator Draw(NavMeshPath path)
     {
         yield return new WaitForEndOfFrame();
-        //line.positionCount = path.corners.Length;//set the array of positions to the amount of corners
+        line.positionCount = path.corners.Length;//set the array of positions to the amount of corners
         for (int i = 0; i < path.corners.Length; i++)
         {
-            Debug.Log(path.corners[i]);
+            //Debug.Log(path.corners[i]);
             //temp.position = path.corners[i];
-            Instantiate(arrow, temp);
-            //line.SetPosition(i, path.corners[i]); //go through each corner and set that to the line renderer's position
+            //Instantiate(arrow, temp);
+            line.SetPosition(i, path.corners[i]); //go through each corner and set that to the line renderer's position
         }
     }
 }
