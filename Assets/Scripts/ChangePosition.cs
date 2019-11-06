a02684bf04b939ad309b2d6e782ba299c3803430
LukasJOfficial <54264562+LukasJOfficial@users.noreply.github.com>
2019-11-06 15:12:11 +0100

    map updated

--- Assets/Scripts/ChangePosition.cs
+++ Assets/Scripts/ChangePosition.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 
 public class ChangePosition : MonoBehaviour
 {
-    public Transform offset;
     public GameObject target;
-    public Transform moveLocation;
-    public bool move = true;
-
 
     public void Move(Transform moveLocation)
     {
         target.transform.position = moveLocation.transform.position;
-    }
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Move(moveLocation);
-        }
     }
 }
