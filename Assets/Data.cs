10a3f986026d88dec8d0e9a7184ede5bce964368
LukasJOfficial <54264562+LukasJOfficial@users.noreply.github.com>
2019-11-13 11:26:21 +0100

    Locker room

---
+++ Assets/Data.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Data : MonoBehaviour
+{
+    public Text x;
+    public Text y;
+    public Text z;
+
+    public GameObject lookForData;
+
+    // Update is called once per frame
+    void Update()
+    {
+        x.text = lookForData.transform.position.x.ToString();
+        y.text = lookForData.transform.position.y.ToString();
+        z.text = lookForData.transform.position.z.ToString();
+    }
+}
