0a40eea946e3068ce8b6dfd917059806c65ad174
Zujevas <38316704+Zujevas@users.noreply.github.com>
2019-11-26 09:37:43 +0100

    cahin smth

--- Assets/Scripts/ChainScript.cs
+++ Assets/Scripts/ChainScript.cs
@@ -9,23 +9,29 @@ public class ChainScript : MonoBehaviour
     private float maxDist;
     private float dist;
     private Vector3 dist1;
+    private Vector3 dist2;
+    int temp = 0;
     void Start()
     {
         maxDist = Vector3.Distance(chain_start.transform.position, chain_end.transform.position);
         //print(maxDist);
 
         dist1 = chain_end.transform.position - chain_start.transform.position;
-        print(dist1);
+       
+        dist2 = new Vector3(chain_end.transform.position.x - chain_start.transform.position.x, 0, chain_end.transform.position.z - chain_start.transform.position.z);
+        print(dist2);
     }
 
 
     void Update()
     {
         dist = Vector3.Distance(chain_start.transform.position, chain_end.transform.position);
-        if (dist > maxDist)
+        if (dist > maxDist && temp == 0)
         {
-            chain_end.transform.position = (chain_start.transform.position + dist1).normalized;
-                //(chain_end.transform.position - chain_start.transform.position).normalized * maxDist + chain_start.transform.position;
+            temp++;
+            chain_end.transform.position = (chain_start.transform.position + dist2);
+            print(chain_end.transform.position);
+            //  chain_end.transform.position= (chain_end.transform.position - chain_start.transform.position).normalized * maxDist + chain_start.transform.position;
         }
     }
 }
