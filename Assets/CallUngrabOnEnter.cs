82e167082e8356d9f1398691269651cf90e5614d
Zujevas <38316704+Zujevas@users.noreply.github.com>
2019-12-06 12:48:24 +0100

    Chain done BITCH!

---
+++ Assets/CallUngrabOnEnter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallUngrabOnEnter : MonoBehaviour
+{
+    public ActivateUngrab ungrabber;
+   
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Wall chain")
+        {
+        ungrabber.ungrab();
+        }
+    }
+}
---
+++ Assets/Scripts/ChainTeleportation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.Interactions.Interactables.Grab;
+
+public class ChainTeleportation : MonoBehaviour
+{
+    private bool isGrabed = false;
+    private ActivateUngrab ungrabber;
+
+    void Start()
+    {
+        ungrabber = gameObject.GetComponent<ActivateUngrab>();
+    }
+    public void ChainGrabed()
+    {
+        isGrabed = true;
+    }
+
+    public void ChainReleased()
+    {
+        isGrabed = false;
+    }
+
+    public void TeleportAction()
+    {
+        if (isGrabed)
+        {
+            ungrabber.ungrab();
+        }
+    }
+
+
+}
